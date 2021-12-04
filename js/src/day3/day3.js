d221a23ba6662a9da988db54e81ed70d792101ff
Mike Ryczkowski <mryczkowski@gmail.com>
2021-12-04 02:36:18 -0800

    Day 3 part 1

---
+++ js/src/day3/day3.js
@@ -0,0 +1,36 @@
+import fs from 'fs';
+import path from 'path';
+
+function readInput() {
+    const buf = fs.readFileSync(path.resolve('src/day3/day3Input.txt'));
+    return buf.toString().split('\n');
+}
+
+function part1() {
+    const nums = readInput();
+    const bitSums = [];
+
+    nums.forEach((numStr) => {
+        for (let i = 0; i < numStr.length; i++) {
+            const bitVal = parseInt(numStr.charAt(i));
+    
+            if (bitSums.length <= i) {
+                bitSums.push(bitVal);
+            } else {
+                bitSums[i] += bitVal;
+            }
+        }
+    });
+
+    const gammaRateStr = bitSums.map((sum) => sum > Math.floor(nums.length / 2) ? 1 : 0).join('');
+    const epsilonRateStr = bitSums.map((sum) => sum > Math.floor(nums.length / 2) ? 0 : 1).join('');
+
+    const gammaRateDecimal = parseInt(gammaRateStr, 2);
+    const epsilonRateDecimal = parseInt(epsilonRateStr, 2);
+
+    console.log('Day 3, part 1:', gammaRateDecimal * epsilonRateDecimal);
+}
+
+export default {
+    part1,
+}
--- js/src/index.js
+++ js/src/index.js
@@ -1,8 +1,11 @@
 import day2 from './day2/day2.js';
+import day3 from './day3/day3.js';
 
 function main() {
-    day2.part1();
-    day2.part2();
+    // day2.part1();
+    // day2.part2();
+
+    day3.part1();
 }
 
 main();
